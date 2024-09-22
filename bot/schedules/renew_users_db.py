12ffcfe57b5e2cf6156c38b34910ee954cb99c58
zwickvitaly <zwickvitaly@gmail.com>
2024-09-22 10:48:23 +0300

    обновлена логика удаления пользователей из базы и из каналов

--- bot/schedules/renew_users_db.py
+++ bot/schedules/renew_users_db.py
@@ -26,52 +26,37 @@ async def renew_users_base(bot: Bot):
                     for user in users_statuses
                     if user.get("telegram_id") and user.get("status_work") is False
                 ]
-                banned_ids = []
                 async with async_session() as session:
-                    async with session.begin():
-                        # ищем уволенных пользователей, оставшихся в бд
-                        q1 = await session.execute(
-                            select(User).where(User.telegram_id.in_(fired_ids))
-                        )
-                        fired_users = list(q1.scalars())
-                        if fired_users:
-                            # если нашли - добавляем их в banned_ids
-                            for user in fired_users:
-                                logger.info(user)
-                                banned_ids.append(user.telegram_id)
-                            logger.info(f"Есть уволенные: {banned_ids}")
-                        else:
-                            logger.info("Уволенных не найдено")
                     async with session.begin():
                         q2 = await session.execute(
                             select(Chat).where(Chat.admin == True)
                         )
                         # получаем чаты, которые бот администрирует
                         chats = list(q2.scalars())
-                    if chats and banned_ids:
+                    if chats and fired_ids:
                         # есть чаты И уволенные
                         logger.info(
                             "Есть чаты, где бот - админ, и пользователи, которых нужно кикнуть."
                         )
                         for chat in chats:
-                            for banned in banned_ids:
+                            for fired in fired_ids:
                                 try:
                                     # пытаемся кикнуть уволенных из чата
-                                    logger.info(f"Удаляем {banned} из чата {chat}")
+                                    logger.info(f"Удаляем {fired} из чата {chat}")
                                     await bot.ban_chat_member(
                                         chat.id,
-                                        banned,
+                                        fired,
                                         until_date=timedelta(seconds=59),
                                     )
                                 except TelegramBadRequest as e:
                                     # не получается - скорее всего у бота нет прав
                                     logger.error(
-                                        f"Нельзя удалить пользователя {banned} из чата {chat.id}. Причина: {e}"
+                                        f"Нельзя удалить пользователя {fired} из чата {chat.id}. Причина: {e}"
                                     )
                         async with session.begin():
                             # удаляем кикнутых пользователей из бд
                             await session.execute(
-                                delete(User).where(User.telegram_id.in_(banned_ids))
+                                delete(User).where(User.telegram_id.in_(fired_ids))
                             )
                         logger.info("Удаление уволенных завершено")
 
