85f47ca06a45db242b5b840d4f9c7abba858ee8a
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 02:31:30 +0300

    fixes

--- bot/schedules/renew_users_db.py
+++ bot/schedules/renew_users_db.py
@@ -35,68 +35,68 @@ async def renew_users_base():
                     fired_users: list[User] = q.scalars()
             fired_ids = [user.telegram_id for user in fired_users]
             logger.info(fired_ids)
-            # for fired_id in fired_ids:
-            #     try:
-            #         # удаляем опросники
-            #         scheduler.remove_job(
-            #             job_id=f"{RedisKeys.SCHEDULES_THREE_MONTHS_KEY}_{fired_id}"
-            #         )
-            #         scheduler.remove_job(
-            #             job_id=f"{RedisKeys.SCHEDULES_TWO_MONTHS_KEY}_{fired_id}"
-            #         )
-            #         scheduler.remove_job(
-            #             job_id=f"{RedisKeys.SCHEDULES_ONE_MONTH_KEY}_{fired_id}"
-            #         )
-            #         scheduler.remove_job(
-            #             job_id=f"{RedisKeys.SCHEDULES_ONE_WEEK_KEY}_{fired_id}"
-            #         )
-            #         scheduler.remove_job(
-            #             job_id=f"{RedisKeys.SCHEDULES_FIRST_DAY_KEY}_{fired_id}"
-            #         )
-            #     except Exception as e:
-            #         logger.error(f"Ошибка при удалении триггеров кронтаба: {e}")
-            #     try:
-            #         # перебрасываем юзера в таблицу уволенных
-            #         await remove_fired_worker_surveys(fired_id)
-            #     except Exception as e:
-            #         logger.error(f"Не получилось удалить данные из гугл-таблицы: {e}")
-            # async with async_session() as session:
-            #     async with session.begin():
-            #         q2 = await session.execute(select(Chat).where(Chat.admin == True))
-            #         # получаем чаты, которые бот администрирует
-            #         chats = list(q2.scalars())
-            #     if chats and fired_ids:
-            #         # есть чаты И уволенные
-            #         logger.info(
-            #             "Есть чаты, где бот - админ, и пользователи, которых нужно кикнуть."
-            #         )
-            #         for chat in chats:
-            #             for fired in fired_ids:
-            #                 try:
-            #                     # пытаемся кикнуть уволенных из чата
-            #                     logger.info(f"Удаляем {fired} из чата {chat.id}")
-            #                     await bot.ban_chat_member(
-            #                         chat.id,
-            #                         fired,
-            #                         until_date=timedelta(seconds=59),
-            #                     )
-            #                 except TelegramBadRequest as e:
-            #                     # не получается - скорее всего у бота нет прав
-            #                     logger.error(
-            #                         f"Нельзя удалить пользователя {fired} из чата {chat.id}. Причина: {e}"
-            #                     )
-            #         async with session.begin():
-            #             # удаляем кикнутых пользователей из бд
-            #             await session.execute(
-            #                 delete(User).where(User.telegram_id.in_(fired_ids))
-            #             )
-            #             await session.commit()
-            #         logger.info("Удаление уволенных завершено")
-            #
-            #     else:
-            #         logger.info(
-            #             "Не найдены чаты, где бот админ, или пользователи, которых нужно забанить"
-            #         )
+            for fired_id in fired_ids:
+                try:
+                    # удаляем опросники
+                    scheduler.remove_job(
+                        job_id=f"{RedisKeys.SCHEDULES_THREE_MONTHS_KEY}_{fired_id}"
+                    )
+                    scheduler.remove_job(
+                        job_id=f"{RedisKeys.SCHEDULES_TWO_MONTHS_KEY}_{fired_id}"
+                    )
+                    scheduler.remove_job(
+                        job_id=f"{RedisKeys.SCHEDULES_ONE_MONTH_KEY}_{fired_id}"
+                    )
+                    scheduler.remove_job(
+                        job_id=f"{RedisKeys.SCHEDULES_ONE_WEEK_KEY}_{fired_id}"
+                    )
+                    scheduler.remove_job(
+                        job_id=f"{RedisKeys.SCHEDULES_FIRST_DAY_KEY}_{fired_id}"
+                    )
+                except Exception as e:
+                    logger.error(f"Ошибка при удалении триггеров кронтаба: {e}")
+                try:
+                    # перебрасываем юзера в таблицу уволенных
+                    await remove_fired_worker_surveys(fired_id)
+                except Exception as e:
+                    logger.error(f"Не получилось удалить данные из гугл-таблицы: {e}")
+            async with async_session() as session:
+                async with session.begin():
+                    q2 = await session.execute(select(Chat).where(Chat.admin == True))
+                    # получаем чаты, которые бот администрирует
+                    chats = list(q2.scalars())
+                if chats and fired_ids:
+                    # есть чаты И уволенные
+                    logger.info(
+                        "Есть чаты, где бот - админ, и пользователи, которых нужно кикнуть."
+                    )
+                    for chat in chats:
+                        for fired in fired_ids:
+                            try:
+                                # пытаемся кикнуть уволенных из чата
+                                logger.info(f"Удаляем {fired} из чата {chat.id}")
+                                await bot.ban_chat_member(
+                                    chat.id,
+                                    fired,
+                                    until_date=timedelta(seconds=59),
+                                )
+                            except TelegramBadRequest as e:
+                                # не получается - скорее всего у бота нет прав
+                                logger.error(
+                                    f"Нельзя удалить пользователя {fired} из чата {chat.id}. Причина: {e}"
+                                )
+                    async with session.begin():
+                        # удаляем кикнутых пользователей из бд
+                        await session.execute(
+                            delete(User).where(User.telegram_id.in_(fired_ids))
+                        )
+                        await session.commit()
+                    logger.info("Удаление уволенных завершено")
+
+                else:
+                    logger.info(
+                        "Не найдены чаты, где бот админ, или пользователи, которых нужно забанить"
+                    )
         else:
             logger.info("Нет инфо по пользователям")
     except Exception as e:
