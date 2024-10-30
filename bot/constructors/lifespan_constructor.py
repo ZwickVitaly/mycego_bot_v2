ae76dd149383201e25438308b130a459bddee90a
zwickvitaly <zwickvitaly@gmail.com>
2024-10-30 14:51:03 +0300

    fixes

--- bot/constructors/lifespan_constructor.py
+++ bot/constructors/lifespan_constructor.py
@@ -29,7 +29,7 @@ async def start_up(bot: Bot):
     await anotify_admins(bot, "Бот запущен", admins_list=ADMINS)
     await renew_users_base()
     await renew_works_base()
-    await fix_surveys_job()
+    # await fix_surveys_job()
     scheduler.add_job(
         happy_birthday,
         trigger=CronTrigger(hour=12, minute=0, second=0, timezone=TIMEZONE),
